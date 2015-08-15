d07a5144be532b828702663c0bbedab6bcff427f
Kilian Sprotte <kilian.sprotte@gmail.com>
2015-08-15 20:55:58 +0200

    Fixed handleToDevNull.

--- src/Main.hs
+++ src/Main.hs
@@ -69,7 +69,7 @@ clispScript imagePath systems = intercalate "\n" lines
 
 -- IO
 handleToDevNull :: IO Handle
-handleToDevNull = openFile "/tmp/log" WriteMode
+handleToDevNull = openFile "/dev/null" WriteMode
 
 makeImage :: String -> [String] -> IO (Either (String, Int) String)
 makeImage imagePath systems = do
